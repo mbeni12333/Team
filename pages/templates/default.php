e334970db1aa643f84b7e8459dd3162ecda16af1
GamuEater <mbeni12333@gmail.com>
2018-04-01 17:38:38 +0200

    after cleaning

---
+++ pages/templates/default.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="container">
+            <?php echo $content;?>
+        </div>
+        <script src="javascript/jquery.js"></script>
+    </body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
-require_once('init.php');
-
-DB::getInstance();
-
-
+    require_once('../core/init.php');
+    // index.php?p=login 
+    // to separate at maximum access to server for security
+    // files are found in ../pages 
+    $p = isset($_GET['p'])) ? $_GET['p'] : 'login';
+    ob_start();
+    if($p === 'login'){
+        require '../pages/login.php';
+    }else if(){
+        require '';
+    }
+    $content = ob_get_clean();
+    require '..pages/template/default.php';
 ?>
